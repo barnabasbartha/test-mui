b7b68c879468294d2869a15d6ff23a656e0d5f63
Barnabás Bartha <barnabas.bartha@hi.health>
2024-07-08 14:15:56 +0200

    Add nx react

---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return <StyledApp>asd</StyledApp>;
+}
+
+export default App;
