586a4149ed858a622fa03f4e2dcfa0a2e3fb21c0
Barnabás Bartha <barnabas.bartha@hi.health>
2024-07-10 19:42:24 +0200

    Fix use client

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,49 @@
+'use client';
+import styled from '@emotion/styled';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Typography,
+} from '@mui/material';
+
+// export const generateStaticParams = () => [{ slug: [''] }];
+
+// export const getStaticProps = async (context) => ({
+//   props: {},
+// });
+
+const StyledApp = styled.div``;
+
 export default function Page() {
-  return <div>asd</div>;
+  return (
+    <StyledApp>
+      <Container>
+        <Card variant="outlined" sx={{ marginY: '1rem' }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="https://next.mui.com/static/images/cards/contemplative-reptile.jpg"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Container>
+    </StyledApp>
+  );
 }
