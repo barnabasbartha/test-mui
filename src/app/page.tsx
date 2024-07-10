150e29fc0e38de906ca6f36e114687b45d6b0e79
Barnabás Bartha <barnabas.bartha@hi.health>
2024-07-10 17:35:35 +0200

    Init nextjs + mui

---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+import { Box } from '@mui/material';
+
+export const generateStaticParams = () => [{ slug: [''] }];
+
+// export const getStaticProps = async (context) => ({
+//   props: {},
+// });
+
+// const StyledApp = styled.div`
+//   Your style here
+// `;
+
+const Page = () => (
+  <Box>asd</Box>
+  // <StyledApp>
+  //   <Container>
+  //     <Card variant="outlined" sx={{ marginY: '1rem' }}>
+  //       <CardMedia
+  //         component="img"
+  //         alt="green iguana"
+  //         height="140"
+  //         image="https://next.mui.com/static/images/cards/contemplative-reptile.jpg"
+  //       />
+  //       <CardContent>
+  //         <Typography gutterBottom variant="h5">
+  //           Lizard
+  //         </Typography>
+  //         <Typography variant="body2" color="text.secondary">
+  //           Lizards are a widespread group of squamate reptiles, with over 6,000
+  //           species, ranging across all continents except Antarctica
+  //         </Typography>
+  //       </CardContent>
+  //       <CardActions>
+  //         <Button size="small">Share</Button>
+  //         <Button size="small">Learn More</Button>
+  //       </CardActions>
+  //     </Card>
+  //   </Container>
+  // </StyledApp>
+);
+
+export default Page;
